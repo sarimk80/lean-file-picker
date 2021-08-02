6b0ce70304164ae3e476173128393140ba788931
Claudio Felber <claudio.felber@perron2.ch>
2021-08-02 21:22:40 +0200

    Implement allowedMimeTypes and listener arguments for iOS

--- README.md
+++ README.md
@@ -2,6 +2,30 @@
 
 Pick a single file using the native file explorer.
 
+## Motivation
+
+Why do we need another file picker for Flutter if there is already a plugin
+for file selection available? The existing [`file_picker`](1) plugin
+has two serious problems on iOS:
+
+- The [`file_picker`](1) plugin requires you to build your Flutter app with
+  frameworks ([`use_frameworks!`](2)) instead of using static linking. This
+  results in substantially larger binaries.
+- The [`file_picker`](1) plugin has a considerable list of large dependencies
+  (DKImagePickerController, DKPhotoGallery, SDWebImage, SwiftyGif) that yet
+  again increase your binary size.
+
+By using `lean_file_picker` instead of [`file_picker`](1) I managed to reduce
+the installation size of an app from 81.3 MB to 31.3 MB — a reduction of 50 MB
+or more than 60%!
+
+If the native file explorer behavior on Android and iOS is sufficient for your
+needs then replace [`file_picker`](1) with `lean_file_picker` for a much
+reduced installation size and memory consumption (especially on iOS).
+
+[1]: https://pub.dev/packages/file_picker
+[2]: https://guides.cocoapods.org/syntax/podfile.html#use_frameworks_bang
+
 ## Usage
 
 ```dart
--- ios/Classes/LeanFilePickerPlugin.m
+++ ios/Classes/LeanFilePickerPlugin.m
@@ -2,43 +2,71 @@
 #import <MobileCoreServices/MobileCoreServices.h>
 #import <objc/runtime.h>
 
-@interface LeanFilePickerPlugin ()<UIDocumentPickerDelegate>
-
-- (NSString *)fileTypeForExtension:(NSString *)extension;
-
+@interface LeanFilePickerPlugin ()<UIDocumentPickerDelegate, FlutterStreamHandler>
 @end
 
-@implementation LeanFilePickerPlugin
+@implementation LeanFilePickerPlugin {
+    FlutterEventSink eventSink;
+}
+
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar> *)registrar {
     FlutterMethodChannel *channel = [FlutterMethodChannel
         methodChannelWithName:@"lean_file_picker"
               binaryMessenger:[registrar messenger]];
     LeanFilePickerPlugin *instance = [[LeanFilePickerPlugin alloc] init];
     [registrar addMethodCallDelegate:instance channel:channel];
+
+    FlutterEventChannel *eventChannel = [FlutterEventChannel
+        eventChannelWithName:@"lean_file_picker_events"
+             binaryMessenger:[registrar messenger]];
+    [eventChannel setStreamHandler:instance];
+}
+
+- (FlutterError *)onListenWithArguments:(id)arguments eventSink:(FlutterEventSink)events {
+    eventSink = events;
+    return nil;
+}
+
+- (FlutterError *)onCancelWithArguments:(id)arguments {
+    eventSink = nil;
+    return nil;
 }
 
 - (void)handleMethodCall:(FlutterMethodCall *)call result:(FlutterResult)result {
     if ([@"pickFile" isEqualToString:call.method]) {
         NSDictionary *arguments = call.arguments;
         NSArray *extensions = arguments[@"allowedExtensions"];
-        NSMutableArray *fileTypes = [[NSMutableArray alloc] initWithCapacity:10];
-        for (NSString *extension in extensions) {
-            NSString *fileType = [self fileTypeForExtension:extension];
-            [fileTypes addObject:fileType];
+        if ([extensions isKindOfClass:[NSNull class]]) {
+            extensions = [NSArray array];
         }
-
-        UIDocumentPickerViewController *documentPicker = [[UIDocumentPickerViewController alloc]
-            initWithDocumentTypes:fileTypes
-                           inMode:UIDocumentPickerModeImport];
-        documentPicker.delegate = self;
-        objc_setAssociatedObject(documentPicker, @"result", result, OBJC_ASSOCIATION_RETAIN);
-
-        UIWindow *window = UIApplication.sharedApplication.delegate.window;
-        UIViewController *controller = window.rootViewController;
-        [controller presentViewController:documentPicker animated:true completion:nil];
+        NSArray *mimeTypes = arguments[@"allowedMimeTypes"];
+        if ([mimeTypes isKindOfClass:[NSNull class]]) {
+            mimeTypes = [NSArray array];
+        }
+        [self pickFile:result extensions:extensions mimeTypes:mimeTypes];
     } else {
         result(FlutterMethodNotImplemented);
     }
+}
+
+- (void)pickFile:(FlutterResult)result extensions:(NSArray<NSString *> *)extensions mimeTypes:(NSArray<NSString *> *)mimeTypes {
+    NSUInteger capacity = extensions.count + mimeTypes.count;
+    NSMutableArray *fileTypes = [[NSMutableArray alloc] initWithCapacity:capacity];
+    [fileTypes addObjectsFromArray:mimeTypes];
+    for (NSString *extension in extensions) {
+        NSString *fileType = [self fileTypeForExtension:extension];
+        [fileTypes addObject:fileType];
+    }
+
+    UIDocumentPickerViewController *documentPicker = [[UIDocumentPickerViewController alloc]
+        initWithDocumentTypes:fileTypes
+                       inMode:UIDocumentPickerModeImport];
+    documentPicker.delegate = self;
+    objc_setAssociatedObject(documentPicker, @"result", result, OBJC_ASSOCIATION_RETAIN);
+
+    UIWindow *window = UIApplication.sharedApplication.delegate.window;
+    UIViewController *controller = window.rootViewController;
+    [controller presentViewController:documentPicker animated:true completion:nil];
 }
 
 - (NSString *)fileTypeForExtension:(NSString *)extension {
